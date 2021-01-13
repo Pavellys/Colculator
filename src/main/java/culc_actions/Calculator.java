3f9ce8207c301bd8bc5ae82d3d2319bfab7cd4d6
Pavellys <dkwr2@mail.ru>
2021-01-13 21:27:34 +0300

    Added new tests

---
+++ src/main/java/culc_actions/Calculator.java
@@ -0,0 +1,20 @@
+package culc_actions;
+
+public class Calculator {
+
+    public int sum(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public int minus(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+}
